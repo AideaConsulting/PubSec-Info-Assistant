2643e9659c66733d5120274903ed33fe5803dbe4
AideaConsulting <jeanpaulfilpo@hotmail.com>
2024-10-15 03:22:38 +0100

    Changed to free tier

--- infra/variables.tf
+++ infra/variables.tf
@@ -209,12 +209,12 @@ variable "openAiSkuName" {
 
 variable "chatGptDeploymentName" {
   type    = string
-  default = "gpt-35-turbo-16k"
+  default = "gpt-4o-mini"
 }
 
 variable "chatGptModelName" {
   type    = string
-  default = "gpt-35-turbo-16k"
+  default = "gpt-4o-mini"
 }
 
 variable "chatGptModelSkuName" {
@@ -225,7 +225,7 @@ variable "chatGptModelSkuName" {
 
 variable "chatGptModelVersion" {
   type    = string
-  default = "0613"
+  default = "2024-07-18"
 }
 
 variable "chatGptDeploymentCapacity" {
@@ -395,13 +395,13 @@ variable "appServicePlanName" {
 variable "appServiceSkuSize" {
   description = "The size of the app service plan for the IA website. Must match with the tier value in appServiceSkuTier."
   type = string
-  default = "S1"
+  default = "F1"
 }
 
 variable "appServiceSkuTier" {
   description = "The tier of the app service plan for the IA website. Must match with the size value in appServiceSkuSize."
   type = string
-  default = "Standard"
+  default = "Free"
   
 }
 
@@ -413,13 +413,13 @@ variable "enrichmentAppServicePlanName" {
 variable "enrichmentAppServiceSkuSize" {
   description = "The size of the app service plan for the enrichment service. Must match with the tier value in enrichmentAppServiceSkuTier."
   type = string
-  default = "P2v3"
+  default = "F1"
 }
 
 variable "enrichmentAppServiceSkuTier" {
   description = "The tier of the app service plan for the enrichment service. Must match with the size value in enrichmentAppServiceSkuSize."
   type = string
-  default = "PremiumV3"
+  default = "Free"
 }
 
 variable "logAnalyticsName" {
@@ -450,13 +450,13 @@ variable "functionsAppName" {
 variable "functionsAppSkuSize" {
   description = "The size of the app service plan for the functions app. Must match with the tier value in functionsAppSkuTier."
   type = string
-  default = "S2"
+  default = "F1"
 }
 
 variable "functionsAppSkuTier" {
   description = "The tier of the app service plan for the functions app. Must match with the size value in functionsAppSkuSize."
   type = string
-  default = "Standard"
+  default = "Free"
 }
 
 variable "searchServicesName" {
